8bf122cd5c5b689076fbb4d94135e332b1c38632
mahesh mahi <mahesh20104@gmail.com>
2025-05-12 22:52:13 +0530

    Fix: removed unused useTheme import to fix Netlify build error

--- src/components/LandingPage.js
+++ src/components/LandingPage.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Box, Typography, Button, useTheme } from '@mui/material';
+import { Box, Typography, Button } from '@mui/material';
 import { styled, keyframes } from '@mui/system';
 
 // Keyframe animations
